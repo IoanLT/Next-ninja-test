62a301abe33a1e1371fd94531043004121de6d09
IoanLT <ioan.tranole@gmail.com>
2021-05-12 18:43:05 +0100

    creating a few components to test the routes and apply some basic global styles

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-		<div>
+	return (
+		<div>			
 			<h1>Homepage</h1>
 			<p>
 				Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -16,6 +19,9 @@ export default function Home() {
 				industry. Lorem Ipsum has been the industry's standard dummy text ever
 				since the 1500s
 			</p>
+			<Link href="/ninjas">
+				<a>See ninja listing</a>
+			</Link>			
 		</div>
 	);
 }
