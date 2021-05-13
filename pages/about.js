6cca40f0cc421d788ff2d5a10a5aa8501e768569
IoanLT <ioan.tranole@gmail.com>
2021-05-13 08:37:35 +0100

    adding the head component andwith a title and meta link

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,26 @@
+import Head from 'next/head'
+
 const About = () => {
   return (
-		<div>
-			<h1>About</h1>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s
-			</p>
-		</div>
+		<>
+			<Head>
+				<title>Ninja list | About</title>
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s
+				</p>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s
+				</p>
+			</div>
+		</>
 	);
 }
  
--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,27 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
-		<div>
-			<h1 className={styles.title}>Homepage</h1>
-			<p className={styles.text}>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s
-			</p>
-			<p className={styles.text}>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s
-			</p>
-			<Link href="/ninjas">
-				<a className={styles.btn}>See ninja listing</a>
-			</Link>
-		</div>
+		<>
+			<Head>
+				<title>Ninja list | Home</title>
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1 className={styles.title}>Homepage</h1>
+				<p className={styles.text}>
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s
+				</p>
+				<p className={styles.text}>
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s
+				</p>
+				<Link href="/ninjas">
+					<a className={styles.btn}>See ninja listing</a>
+				</Link>
+			</div>
+		</>
 	);
 }
